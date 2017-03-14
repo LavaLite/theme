32969e2599c7beffa3d44b7a044c2667e0bc32e2
George John <george@renfos.com>
2017-03-14 16:53:07 +0530

    Update composers.

--- build/pie-split.sh
+++ build/pie-split.sh
@@ -2,7 +2,7 @@
 # This will split up each Lavalite theme to its own github repo
 #
 
-./git-subsplit.sh init git@github.com:lavalite/theme.git
+./git-subsplit.sh init git@github.com:lavalite/themes.git
 ./git-subsplit.sh publish admin:git@github.com:Litecms/Admin.git
 ./git-subsplit.sh publish user:git@github.com:Litecms/User.git
 ./git-subsplit.sh publish public:git@github.com:Litecms/Public.git
