26c3d1b749771b62e65e313eb5e7e8eb10b29cfd
George John <george@renfos.com>
2018-05-28 15:01:29 +0530

    Meta title added.

--- public/layouts/home.blade.php
+++ public/layouts/home.blade.php
@@ -5,7 +5,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>{{ Theme::getTitle() }} - {{__('app.name')}}</title>
-        <meta name="description" content="The Lavalite Content Management System">
+        <meta name="keyword" content="{{ Theme::getMetaKeyword() }}">
+        <meta name="description" content="{{ Theme::getMetaDescription() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {!! Theme::asset()->styles() !!}
         {!! Theme::asset()->scripts() !!}
