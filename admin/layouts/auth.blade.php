d62695f10eae7249fb891242900aef35cb98c5f2
George John <george@renfos.com>
2018-10-31 09:54:04 +0530

    Updated admin themes.

--- admin/layouts/auth.blade.php
+++ admin/layouts/auth.blade.php
@@ -5,6 +5,13 @@
         <title>{!! Theme::getMetaTitle() !!} :: {{__('app.name')}}</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <style>
+            html {
+                display: table;
+                width: 100%;
+                height: 100%;
+            }
+        </style>
         {!! Theme::asset()->styles() !!}
         {!! Theme::asset()->scripts() !!}
 
@@ -13,12 +20,5 @@
     {!! Theme::content() !!}
 
     {!! Theme::asset()->container('footer')->scripts() !!}
-    <style>
-        html {
-            display: table;
-            width: 100%;
-            height: 100%;
-        }
-    </style>
     </body>
 </html>
