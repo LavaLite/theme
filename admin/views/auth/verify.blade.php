13b02e090761e3464c72b5cfc39f84a7cab76d68
George John <george@renfos.com>
2019-09-14 22:26:12 +0530

    Upgrade for laravel 6.0

--- admin/views/auth/verify.blade.php
+++ admin/views/auth/verify.blade.php
@@ -6,14 +6,16 @@
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
                     {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('guard.verification.resend', ['guard' => 'admin']) }}">{{ __('click here to request another') }}</a>.
+                    {{ __('If you did not receive the email') }},
+
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                            {{ __('click here to request another') }}
+                        </button>.
+                    </form>
                 </div>
             </div>
         </div>
--- client/views/auth/verify.blade.php
+++ client/views/auth/verify.blade.php
@@ -6,15 +6,17 @@
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
+                {{ __('Before proceeding, please check your email for a verification link.') }}
+                {{ __('If you did not receive the email') }},
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('guard.verification.resend', ['guard' => 'client']) }}">{{ __('click here to request another') }}</a>.
-                </div>
+                <form class="d-inline" method="POST"
+                    action="{{ route('guard.verification.resend', ['guard' => 'client']) }}">
+                    @csrf
+
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                        {{ __('click here to request another') }}
+                    </button>.
+                </form>
             </div>
         </div>
     </div>
