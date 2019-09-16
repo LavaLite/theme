db6de71e946e76ab12c9ef9f10f5d44272569bf0
George John <george@renfos.com>
2019-09-16 22:35:14 +0530

    Removed user theme.

--- build/pie-split.sh
+++ build/pie-split.sh
@@ -4,7 +4,6 @@
 
 ./git-subsplit.sh init git@github.com:lavalite/themes.git
 ./git-subsplit.sh publish admin:git@github.com:Litecms/Admin.git
-./git-subsplit.sh publish user:git@github.com:Litecms/User.git
 ./git-subsplit.sh publish public:git@github.com:Litecms/Public.git
 ./git-subsplit.sh publish public:git@github.com:Litecms/Client.git
 rm -rf .subsplit/
