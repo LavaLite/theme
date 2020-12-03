5fe5f8484e2341d806c42e481d670d17a8622a10
George John <george@renfos.com>
2020-12-03 22:42:13 +0400

    Removed client theme

--- client/partials/footer.blade.php
+++
@@ -1,7 +0,0 @@
-        <footer class="main-footer container-fluid">
-            <div class="row mt-20 mb-20">
-                  <div class="col-md-12 text-center">
-                    <p class="copyrights">{!!__('app.rights')!!}</p>
-                  </div>
-            </div>
-        </footer>
